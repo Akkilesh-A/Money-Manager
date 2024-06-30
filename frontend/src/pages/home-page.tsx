d95aa60c798d6e23d9175330c28e3776ac2a4ba4
Akkilesh-A <akkilalagar05@gmail.com>
2024-07-01 00:00:00 +0530

    setup frontend, add tailwind, shadcn, prettier✨

---
+++ frontend/src/pages/home-page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui";
+
+const HomePage = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      Tailwind is working!
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
