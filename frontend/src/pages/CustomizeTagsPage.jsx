233ab0596ef9e8e486d747b0e9f939d169defae1
Akkilesh-A <akkilalagar05@gmail.com>
2024-07-16 13:48:59 +0530

    added update tag modal

--- frontend/src/pages/CustomizeTagsPage.jsx
+++ frontend/src/pages/CustomizeTagsPage.jsx
@@ -6,7 +6,7 @@ import { faTag, faTags, faXmark, faPen, faSquarePlus } from '@fortawesome/free-s
 
 const CustomizeTagsPage = () => {
   const [deleteClicked,setDeleteClicked] = useState(false)
-  const [editClicked,setEditClicked] =useState(false)
+  const [updateClicked,setUpdateClicked] =useState(false)
   const [addClicked,setAddClicked] = useState(false)
   const [tagName,setTagName] =useState("")
   const tags=useSelector(state=>state.tags)
@@ -17,10 +17,11 @@ const CustomizeTagsPage = () => {
       <PageWrapper>
         <div className={`flex w-[100%] flex-wrap flex-col gap-8 `}>
           <div className='absolute top-[40%] left-[40%]'>
-            <DeleteTagModal setDeleteClicked={setDeleteClicked} deleteClicked={deleteClicked} tagName={tagName}/>
+            <DeleteTagModal setDeleteClicked={setDeleteClicked} deleteClicked={deleteClicked} tagName={tagName} />
             <AddNewTags addClicked={addClicked}/>
+            <UpdateTag tag={tagName} updateClicked={updateClicked} />
           </div>
-          <AvailableTags setDeleteClicked={setDeleteClicked} setTagName={setTagName} tags={tags}/>
+          <AvailableTags setDeleteClicked={setDeleteClicked} setTagName={setTagName} tags={tags} setUpdateClicked={setUpdateClicked}/>
           <div className='flex justify-end text-[4rem]'>
             <FontAwesomeIcon onClick={()=>setAddClicked(!addClicked)} className=" text-purple-600 p-1 rounded-xl" icon={faSquarePlus} />
           </div>
@@ -34,7 +35,7 @@ const CustomizeTagsPage = () => {
   )
 }
 
-function AvailableTags({setDeleteClicked,setTagName,tags}){
+function AvailableTags({setDeleteClicked,setTagName,tags,setUpdateClicked}){
   return(
     <div className='flex-1 flex flex-col gap-8 p-4'>
       <div className='flex gap-4 justify-center items-center text-[2rem] font-bold'>Tags <FontAwesomeIcon icon={faTags} /></div>
@@ -44,7 +45,7 @@ function AvailableTags({setDeleteClicked,setTagName,tags}){
             <div key={index} className='flex items-center rounded-xl text-purple-600 bg-purple-200 border-purple-600 border-2 font-semibold text-center'>
               <h3 className='p-1 px-4 text-[1.2rem]'>{tag}</h3> 
               <div  onClick={()=>{
-                setDeleteClicked(true)
+                setUpdateClicked(true)
                 setTagName(tag)
               }} className='cursor-pointer p-2 text-[1.3rem] px-4 rounded-l-xl border-r-purple-600 bg-white'>
               <FontAwesomeIcon icon={faPen} />
@@ -77,21 +78,16 @@ function AddNewTags({addClicked}){
   )
 }
 
-function UpdateTag({tags}){
+function UpdateTag({updateClicked,tag}){
   return (
-    <div className='flex-1 flex flex-col gap-8 rounded-md shadow bg-white p-4'>
-      <div className='flex gap-4 justify-center items-center text-[2rem] font-bold'>Update Tag<FontAwesomeIcon icon={faTag} /></div>
+    <div>
+      {updateClicked && <div className='flex-1 flex flex-col gap-8 rounded-md shadow bg-white p-4'>
+      <div className='flex gap-4 justify-center items-center text-[2rem] font-bold'>Update Tag - {tag}<FontAwesomeIcon icon={faTag} /></div>
       <div className='flex flex-col gap-8 justify-center items-center'>
-        <select type="text" className='min-w-[60%] p-2 text-[1.2rem] text-purple-600 rounded-md bg-purple-200 border-2 border-purple-600' >
-          {tags.map((tag,index)=>{
-            return(
-              <option key={index} >{tag}</option>
-            )
-          })}
-        </select>
-        <input type="text" placeholder='Enter Tag Name' className='min-w-[60%] p-2 text-[1.2rem] text-purple-600 rounded-md bg-purple-200 border-2 border-purple-600' />
+        <input type="text"  placeholder='Enter Tag Name' className='min-w-[60%] p-2 text-[1.2rem] text-purple-600 rounded-md bg-purple-200 border-2 border-purple-600' />
         <button className='min-w-[30%] px-4 py-2 border bg-yellow-400 rounded-md font-semibold'>Update Tag</button>
       </div>
+    </div>}
     </div>
   )
 }
