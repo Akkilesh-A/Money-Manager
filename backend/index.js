f6322d269c8db085fc0e54e9be4b94c69bc7ab50
Akkilesh-A <akkilalagar05@gmail.com>
2024-10-01 22:17:31 +0530

    feat: db connection setup done

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,23 @@
-import express from 'express';
+import express, { Router } from 'express';
+import cors from 'cors';
 import { configDotenv } from 'dotenv';
+import connectDB from './db/connection.js';
 
 const app=express()
 app.use(express.json())
 configDotenv()
+app.use(cors())
+connectDB()
 
 app.get("/",(req,res)=>{
     res.status(200).json({
         message:`Healthy server running ${process.env.PORT}`
     })
 })
+
+const router=Router()
+
+app.use("/api/v1",router)
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
