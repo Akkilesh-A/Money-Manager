272fa5465118c3f1326457c3bde31f4384e34282
Akkilesh-A <akkilalagar05@gmail.com>
2024-10-21 00:00:31 +0530

    refactor: remove StrictMode to prevent two api calls on mount

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,11 +6,11 @@ import { Provider } from "react-redux"
 import { store } from "./app/store.ts"
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <div>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>,
+  </div>
 )
